4d12cb7d20dc16fe66d0f10ff2a31d38c81039e6
fratso <fratsinfo@gmail.com>
2020-03-22 11:28:33 +0100

    BASIC authentication to bypass

--- launch.sh
+++ launch.sh
@@ -3,8 +3,11 @@
 
 DATABASE=AUTISSIER
 tomcat=tomcat/webapps/
-warfile=autissier.war
+warfile=autissier
 
+rm -rf $tomcat$warfile
+rm $tomcat$warfile.war
+mkdir $tomcat$warfile
 cd build
 # infra cfg
 sed s/\<DATABASE\>/$DATABASE/g adapt_this/docker-compose.yml.bak > ../docker-compose.yml
@@ -14,9 +17,11 @@ sed s/\<DATABASE\>/$DATABASE/g adapt_this/list.jsp.bak > src/list.jsp
 sed s/\<DATABASE\>/$DATABASE/g adapt_this/userSafe.jsp.bak > src/userSafe.jsp
 sed s/\<DATABASE\>/$DATABASE/g adapt_this/userUnsafe.jsp.bak > src/userUnsafe.jsp
 cd src
-jar -cvf $warfile *
-mv $warfile ../../$tomcat
+jar -cvf $warfile.war *
+mv $warfile.war ../../$tomcat
 cd ../..
+
+cp -r build/src/tomcat/conf tomcat/conf
 
 sudo docker-compose rm
 sudo docker-compose up --build --remove-orphans
